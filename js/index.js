5c731ced5fd3f1877be9878755a4b8abdf04deb6
liuchengze <760319417@qq.com>
2017-06-28 20:57:21 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by 成成 on 2017/6/26.
+ */
+$(function(){
+    var mySwiper = new Swiper('.swiper-container',{
+        direction : 'vertical',
+        onInit: function(swiper){ //Swiper2.x的初始化是onFirstInit
+            swiperAnimateCache(swiper); //隐藏动画元素
+            swiperAnimate(swiper); //初始化完成开始动画
+        },
+        onSlideChangeEnd: function(swiper){
+            swiperAnimate(swiper); //每个slide切换结束时也运行当前slide动画
+        }
+    })
+});
